d5e83d964b08ac3ff95f74c86bf9d081b5c77463
John Xiong <xiaoyang1258@yeah.net>
2022-02-09 03:26:29 +0000

    caddy-pprof: update go.mod

--- pprof.go
+++ pprof.go
@@ -2,8 +2,10 @@ package pprof
 
 import (
 	"net/http"
-	_ "net/http/pprof"
+	"net/http/pprof"
 	"strings"
+
+	_ "github.com/google/pprof/driver"
 
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
@@ -17,7 +19,9 @@ func init() {
 }
 
 // Handler implements an HTTP handler that ...
-type Handler struct{}
+type Handler struct{
+	ServeMux *http.ServeMux
+}
 
 // CaddyModule returns the Caddy module information.
 func (Handler) CaddyModule() caddy.ModuleInfo {
@@ -27,10 +31,21 @@ func (Handler) CaddyModule() caddy.ModuleInfo {
 	}
 }
 
+// Provision implements caddy.Provisioner.
+func (m *Handler) Provision(ctx caddy.Context) error {
+	m.ServeMux = http.NewServeMux()
+	m.ServeMux.HandleFunc("/debug/pprof/", pprof.Index)
+	m.ServeMux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	m.ServeMux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	m.ServeMux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	m.ServeMux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	return nil
+}
+
 // ServeHTTP implements caddyhttp.MiddlewareHandler.
 func (m *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) (err error) {
 	if strings.HasPrefix(r.URL.Path, "/debug/pprof/") {
-		http.DefaultServeMux.ServeHTTP(w, r)
+		m.ServeMux.ServeHTTP(w, r)
 		return
 	}
 	return next.ServeHTTP(w, r)
